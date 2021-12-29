45e1e52e2eb5efd75bb6e90d38b6473ab289d42d
Ulanbek Bolotbekov <drreal@mail.ru>
2021-12-29 20:55:13 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RemoveSubstring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Строка Админ удалил привет Инструменты новый инструментарий про Строка котиков привет";
+            string[] strings = str.Split(new string[] { " ", "Строка", "Инструменты", "привет" }, StringSplitOptions.RemoveEmptyEntries);
+            string result = string.Join(' ', strings);
+            Console.WriteLine(result);         
+        }
+    }
+}
